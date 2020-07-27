23612200080df9b43ed17257696b3cd3a4560fad
phanivelli <vdurgaphanikumar@gmail.com>
2020-07-27 16:06:09 +0530

    un used import statements removed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import TransactionList from "./components/TransactionList";
-import { createStore } from "redux";
-import { transactionReducer } from "./reducers/transactionReducers";
 
 function App() {
   return <TransactionList />;
